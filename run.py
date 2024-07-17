15494740e10c5d769a06b11509b56e5249d3efb7
Christopher Dunne <christopherdunne@outlook.ie>
2024-07-17 13:50:33 +0000

    Add flavourtext to forest 1a

--- run.py
+++ run.py
@@ -1528,37 +1528,43 @@ def forest_start():
                 slow_screen_clear()
                 town()
                 return False
-    # leads to forest1a
 
 
 def forest_room1a():
-    slow_print("flavour text for room 1a")
     while True:
-        os.system('clear')
+        slow_print("A few animals dash into the bushes\n"
+                   "as you walk into a small clearing.")
         slow_print("What will you do?\n")
-        slow_print("1. Go North.") # 2a
-        slow_print("2. Go West") # 1b
-        slow_print("3. Go South.") # forest start
+        slow_print("1. Go North.")
+        slow_print("2. Go West")
+        slow_print("3. Go South.")
         slow_print("4. Look around.")
-        choice = int(input())
+        choice = input()
         try:
-            if choice != 1 and choice != 2 \
-             and choice != 3 and choice != 4:
+            if choice != str(1) and choice != str(2) \
+             and choice != str(3) and choice != str(4):
                 raise Exception
         except Exception:
-            print("Please enter only 1, 2, 3 or 4.\n")
+            slow_print("Please enter only 1, 2, 3 or 4.")
         else:
-            if choice == 1:
+            if choice == str(1):
+                slow_print("You head north.")
+                slow_screen_clear()
                 forest_room2a()
                 return False
-            elif choice == 2:
+            elif choice == str(2):
+                slow_print("You head west.")
+                slow_screen_clear()
                 forest_room1b()
                 return False
-            elif choice == 3:
+            elif choice == str(3):
+                slow_print("You head south.")
+                slow_screen_clear()
                 forest_start()
                 return False
-            elif choice == 4:
-                slow_print("flavour text for looking")
+            elif choice == str(4):
+                slow_print("The bushes rustle as some critters scurry"
+                           "around in the underbrush.")
 
 
 def forest_room1b():
