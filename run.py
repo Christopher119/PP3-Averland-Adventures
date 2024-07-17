0ee8fbae1a73d1657b74e95958953e9e543b9174
Christopher Dunne <christopherdunne@outlook.ie>
2024-07-17 14:25:47 +0000

    Add flavour text to forest 2b

--- run.py
+++ run.py
@@ -1702,34 +1702,43 @@ def forest_room2a():
 
 
 def forest_room2b():
-    slow_print("flavour text for room 2b")
+
+    random_battle(random_enemy("Forest"), 3)
+
     while True:
-        os.system('clear')
+        slow_print("The trees are particularly dense in this part of the \n"
+                   "forest, making it difficult to move quickly.")
         slow_print("What will you do?\n")
-        slow_print("1. Go North.") # 3b
-        slow_print("2. Go East") # 2a
-        slow_print("3. Go South.") # 1b
+        slow_print("1. Go North.")
+        slow_print("2. Go East")
+        slow_print("3. Go South.")
         slow_print("4. Look around.")
-        choice = int(input())
+        choice = input()
         try:
-            if choice != 1 and choice != 2 \
-             and choice != 3 and choice != 4:
+            if choice != str(1) and choice != str(2) \
+             and choice != str(3) and choice != str(4):
                 raise Exception
         except Exception:
             print("Please enter only 1, 2, 3, or 4.\n")
         else:
-            if choice == 1:
+            if choice == str(1):
+                slow_print("You head North.")
+                slow_screen_clear()
                 forest_room3b()
                 return False
-            elif choice == 2:
+            elif choice == str(2):
+                slow_print("You head East.")
+                slow_screen_clear()
                 forest_room2a()
                 return False
-            elif choice == 3:
+            elif choice == str(3):
+                slow_print("You head West.")
+                slow_screen_clear()
                 forest_room1b()
                 return False
-            elif choice == 4:
-                slow_print("flavour text for looking")
-    # forest 2a or 3b
+            elif choice == str(4):
+                slow_print("You notice more of those marking on the "
+                           "trees as you move between them.")
 
 
 def forest_room2c():
