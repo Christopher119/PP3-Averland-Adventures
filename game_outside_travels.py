990f0824e07aba81a13e98fdebeff43649439b4f
Christopher Dunne <christopherdunne@outlook.ie>
2024-07-18 15:03:51 +0000

    Add comments and docstring to game_outside_travels

--- game_outside_travels.py
+++ game_outside_travels.py
@@ -1,5 +1,12 @@
+# This script was necessary to avoid cyclical
+# imports between the road/forest and town
+# when trying to and from either area.
 import game_hubworld
 
 
 def move_to_town():
+    """
+    This function is simply a helper function to travel
+    to the town from either the road or forest.
+    """
     game_hubworld.town()
